a4a78768c586f773e54ffb1a7a8eb5bac9893c1b
DaviDasVila <you@example.com>
2026-06-25 08:36:06 -0300

    Função venda melhorada

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 import mysql.connector
 
 from banco_dados import iniciar_db, abrir_conexao
-from interfaces.interface import exibir_menu, exibir_checkin
 from comandos.checkin import checkin
+from comandos.carrinho import carrinho_venda
+# from comandos.consultas import consulta_imc
 
 # importação dos submenus de cada função
 from interfaces.submenu import submenu_cad, submenu_est, submenu_consulta
 from interfaces.funcontinuar import exibir_submenuHome
+from interfaces.interface import exibir_menu
 
 
 #iniciar o programa
@@ -15,12 +17,7 @@ iniciar_db()
 
 continuar = 1 #-> inicialização base do menu de continuidade
 while True:
-    if continuar == 2: #-> Se o usuário escolheu sair (parar de continuar)
-        print(f"Caixa Atual: {caixa:.2f}\nEncerrando o Sistema...")
-        break
-    elif continuar == 0: #-> Se o usuário digitou o comando errado no submenu de continuidade
-        continuar = exibir_submenuHome()
-        continue
+
     try:
         conexao = abrir_conexao()
         cursor = conexao.cursor()
@@ -37,6 +34,13 @@ while True:
             cursor.close()
             conexao.close()
     
+    if continuar == 2: #-> Se o usuário escolheu sair (parar de continuar)
+        print(f"Caixa Atual: {caixa:.2f}\nEncerrando o Sistema...")
+        break
+    elif continuar == 0: #-> Se o usuário digitou o comando errado no submenu de continuidade
+        continuar = exibir_submenuHome()
+        continue
+    
     exibir_menu()
     try:
         menu = int(input("Digite o comando: "))
@@ -49,38 +53,47 @@ while True:
         print(f"Caixa Atual: {caixa:.2f}\nEncerrando o Sistema...")
         break
 
-    # responsa davi
     elif menu == 1:
-        checkin()
+        checkin() #FINALIZADA
     
     elif menu == 2:
         print("\n-----Cadastros/Alterações Cadastrais-----\n")
-        submenu_cad()
+        submenu_cad() #FINALIZADA
     
     elif menu == 3:
         print("\n-----Vendas-----\n")
+        carrinho_venda() #iniciado
 
     elif menu == 4:
         print("\n-----Repor Estoque-----\n")
-        submenu_est()
+        submenu_est() #em alterações
     
     elif menu == 5:
-        print("\n-----Consulta-----\n")
-        submenu_consulta()
+        print("\n-----Listar Informações-----\n")
+        #   submenu_listar()
 
     elif menu == 6:
-        print("\n-----Calcular IMC-----")
-    
+        print("\n-----Consulta-----\n")
+        submenu_consulta() #em alterações
+
     elif menu == 7:
-        print("\n-----Controle Financeiro-----\n")   
-    
+        print("\n-----Ativar/Desativar Cadastros-----")
+        # submenu_atv_dst()
+
     elif menu == 8:
+        print("\n-----Calcular IMC-----")
+        # consulta_imc()
+    
+    elif menu == 9:
+        print("\n-----Controle Financeiro-----\n")   
+        #não iniciada
+    
+    elif menu == 10:
         print("\n-----Exportar Relatórios-----\n")
+        #não iniciada
 
     else:
         print("ERRO: Operação Inválida")
     
     continuar = exibir_submenuHome() #-> Menu de Continuidade
-
-
-
+    continue
